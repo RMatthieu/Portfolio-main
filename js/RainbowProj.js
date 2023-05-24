3b451c5b13c3b2609fabe433304501643d7d153a
Matthieu RAVIER <matthieu.ravier@etu.univ-grenoble-alpes.fr>
2023-05-24 14:21:37 +0200

    Mise a jour + rainbow

--- js/RainbowProj.js
+++ js/RainbowProj.js
@@ -1,11 +1,12 @@
 // JavaScript
-const rainbowText = document.querySelector('#rainbow-text');
+const rainbowText = document.querySelector('#Rainbow-text');
 let hue = 0;
-let speed = 0.3;
+let speed = 0.5;
 
 function animateRainbowText() {
   hue += speed;
-  rainbowText.style.color = `hsl(${hue}, 100%, 50%)`;
+  rainbowText.style.backgroundImage = `linear-gradient(to right, hsl(${hue}, 100%, 50%), hsl(${hue + 60}, 100%, 50%), hsl(${hue + 120}, 100%, 50%), hsl(${hue + 180}, 100%, 50%), hsl(${hue + 240}, 100%, 50%), hsl(${hue + 300}, 100%, 50%), hsl(${hue + 360}, 100%, 50%))`;
+  //rainbowText.style.backgroundImage = 'black';
 
   requestAnimationFrame(animateRainbowText);
 }
